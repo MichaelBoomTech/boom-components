a34a037c0a7ff8ffe45a02e5686a64ad190fd82e
NavasardianMichael <navasardianmichael@gmail.com>
2021-05-16 17:13:14 +0400

    add to calendar components added

---
+++ AddToCalendarIcons/src/App.js
@@ -0,0 +1,11 @@
+import AddToCalendarIcons from './lib/index';
+
+function App() {
+  return (
+    <div className="App">
+      <AddToCalendarIcons id={987098766} />
+    </div>
+  );
+}
+
+export default App;
---
+++ AddToCalendarIcons/src/lib/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function AddToCalendarIcons(props) {
+
+   return (
+      <h1>{props.id}dwdwa</h1>
+   )
+}
+export default AddToCalendarIcons;
