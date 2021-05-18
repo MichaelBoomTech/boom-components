67341d5b5771df17eb60185b63e18ea669fe6a14
Miq <navasardianmichael@gmail.com>
2021-05-18 18:30:00 +0400

    module created

---
+++ src/components/AddToCalendarIcons/dist/helpers.js
@@ -0,0 +1,194 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.downloadSharer = downloadSharer;
+exports.openAddToUrl = openAddToUrl;
+exports.setLocation = setLocation;
+exports.openShareUrl = openShareUrl;
+exports.generateEventUrl = generateEventUrl;
+exports.copyLink = copyLink;
+exports.encodeShareUrl = void 0;
+
+require("core-js/modules/es.string.replace.js");
+
+require("core-js/modules/es.string.split.js");
+
+function downloadSharer(e, type, event) {
+  e.stopPropagation();
+  console.log(event.endTime);
+  let desc = "".concat(event.desc ? "".concat(event.desc.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&nbsp;/g, " "), "  ") : "").concat(event.venue.name || event.venue.phone || event.venue.email || event.venue.website ? "<p><b>Venue Details.</b></p>  " : "").concat(event.venue.name ? "".concat(event.venue.name, ",<br/>  ") : "").concat(event.venue.phone ? "".concat(event.venue.phone, ",<br/>  ") : "").concat(event.venue.email ? "".concat(event.venue.email, ",<br/>  ") : "").concat(event.venue.website ? "".concat(event.venue.website, ".<br/>  ") : "").concat(event.organizer.name || event.organizer.phone || event.organizer.email || event.organizer.website ? "<p><b>Organizer</b></p>  " : "").concat(event.organizer.name ? "".concat(event.organizer.name, ",<br/>  ") : "").concat(event.organizer.phone ? "".concat(event.organizer.phone, ",<br/>  ") : "").concat(event.organizer.email ? "".concat(event.organizer.email, ",<br/>  ") : "").concat(event.organizer.website ? "".concat(event.organizer.website, ".<br/>  ") : "");
+  let icsSharer = "https://calendar.boomte.ch/createIcsFile?title=".concat(event.title, "&desc=").concat(encodeURIComponent(type === 'icalendar' ? desc.replace(/(<([^>]+)>)/ig, '') : desc), "&start=").concat(event.start, "&end=").concat(event.end, "&address=").concat(encodeURIComponent(event.venue.address));
+  window.location.href = icsSharer;
+}
+
+function openAddToUrl(e, type, event) {
+  e.stopPropagation();
+  let eventDescription = event.desc ? createDesc(event) : "";
+  let url;
+
+  switch (type) {
+    case 'google':
+      if (event.all_day) url = 'https://calendar.google.com/calendar/r/eventedit?text=' + encodeURIComponent(event.title) + '&dates=' + moment(formatForAddtoCalendar(event, 'start', type)).format('YYYYMMDD') + "/" + moment(formatForAddtoCalendar(event, 'end')).format('YYYYMMDD') + '&details=' + encodeURIComponent(event.desc ? eventDescription : "") + '&location' + setLocation(event, 'encode') + '&sprop=name';else url = 'https://calendar.google.com/calendar/r/eventedit?text=' + encodeURIComponent(event.title) + '&dates=' + moment(formatForAddtoCalendar(event, 'start', type)).format('YYYYMMDD[T]HHmmss') + '/' + moment(formatForAddtoCalendar(event, 'end')).format('YYYYMMDD[T]HHmmss') + '&details=' + encodeURIComponent(event.desc ? eventDescription : "") + '&location' + setLocation(event, 'encode') + '&sprop=name';
+      break;
+
+    case 'yahoo':
+      if (event.all_day) url = 'https://calendar.yahoo.com/?v=60&view=d&type=20&DUR=' + (event.all_day ? 'all_day' : '') + '&TITLE=' + encodeURIComponent(event.title) + '&ST=' + moment(formatForAddtoCalendar(event, 'start', type)).format('YYYYMMDD') + "&ET=" + moment(formatForAddtoCalendar(event, 'end', 'yahoo')).format('YYYYMMDD') + '&DESC=' + eventDescription + '&in_loc=' + setLocation(event);else url = 'https://calendar.yahoo.com/?v=60&view=d&type=20&TITLE=' + encodeURIComponent(event.title) + '&ST=' + moment(formatForAddtoCalendar(event, 'start', type)).format('YYYYMMDD[T]HHmmss') + "&ET=" + moment(formatForAddtoCalendar(event, 'end')).format('YYYYMMDD[T]HHmmss') + '&DESC=' + eventDescription + '&in_loc=' + setLocation(event);
+      break;
+
+    default:
+      console.error('undefined calendar type');
+  }
+
+  window.open(url, '_blank');
+  return;
+}
+
+function formatForAddtoCalendar(event, key, type) {
+  let fullStart, fullEnd;
+
+  if (event.all_day) {
+    fullStart = moment(event.start).format('YYYY-MM-DD');
+
+    if (event.end !== event.start) {
+      fullEnd = moment(event.end).add(1, 'days').format('YYYY-MM-DD');
+    } else {
+      fullEnd = moment(event.end).format('YYYY-MM-DD');
+      if (type !== "yahoo") fullEnd = moment(event.end).add(1, 'days').format('YYYY-MM-DD');
+    }
+  } else {
+    fullStart = moment(event.start).format('YYYY-MM-DDTHH:mm:ss');
+    fullEnd = moment(event.end).format('YYYY-MM-DDTHH:mm:ss');
+  }
+
+  if (type === 'yahoo' && event.end !== event.start) fullEnd = moment(fullEnd).subtract(1, 'day').format('YYYY-MM-DDTHH:mm:ss');
+  return key === 'start' ? fullStart : fullEnd;
+}
+
+function setLocation(event, key) {
+  let toGmapLinkBase = 'http://maps.google.com/?q=';
+
+  if (event.location) {
+    if (key === 'encode') return encodeURI(toGmapLinkBase + event.location);else return event.location;
+  }
+
+  let venue = event.venue;
+
+  if (venue && venue.address) {
+    if (key === 'encode') return encodeURI(toGmapLinkBase + venue.address + "+" + venue.city + "+" + venue.statesList + "+" + venue.postal + "+" + venue.country + "+");else return venue.address + ' ' + venue.city + ' ' + venue.statesList + ' ' + venue.postal + ' ' + venue.country;
+  }
+
+  return '';
+}
+
+const encodeShareUrl = str => {
+  let id = str.split("");
+  let i;
+
+  for (i = 0; i < id.length; i++) {
+    switch (id[i]) {
+      case "1":
+        id[i] = "H9";
+        break;
+
+      case "2":
+        id[i] = "A8";
+        break;
+
+      case "3":
+        id[i] = "J7";
+        break;
+
+      case "4":
+        id[i] = "M6";
+        break;
+
+      case "5":
+        id[i] = "R5";
+        break;
+
+      case "6":
+        id[i] = "O4";
+        break;
+
+      case "7":
+        id[i] = "L3";
+        break;
+
+      case "8":
+        id[i] = "W2";
+        break;
+
+      case "9":
+        id[i] = "U1";
+        break;
+
+      case "0":
+        id[i] = "K0";
+        break;
+
+      default:
+        return;
+    }
+  }
+
+  let l = id.length;
+  let chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  for (i = 0; i < l * 2 + 2; i = i + Math.ceil(Math.random() * 3)) {
+    id.splice(i, 0, chars[Math.ceil(Math.random() * 26)]);
+  }
+
+  return id.join('');
+};
+
+exports.encodeShareUrl = encodeShareUrl;
+
+const createDesc = event => "".concat(event.desc ? "".concat(event.desc, "%0D%0A%0D%0A") : "").concat(event.venue.name || event.venue.phone || event.venue.email || event.venue.website ? "Venue Details%0D%0A%0D%0A" : "").concat(event.venue.name ? "".concat(event.venue.name, "%0D%0A") : "").concat(event.venue.phone ? "".concat(event.venue.phone, "%0D%0A") : "").concat(event.venue.email ? "".concat(event.venue.email, "%0D%0A") : "").concat(event.venue.website ? "".concat(event.venue.website, "%0D%0A%0D%0A") : "").concat(event.organizer.name || event.organizer.phone || event.organizer.email || event.organizer.website ? "Organizer%0D%0A%0D%0A" : "").concat(event.organizer.name ? "".concat(event.organizer.name, "%0D%0A") : "").concat(event.organizer.phone ? "".concat(event.organizer.phone, "%0D%0A") : "").concat(event.organizer.email ? "".concat(event.organizer.email, "%0D%0A") : "").concat(event.organizer.website ? "".concat(event.organizer.website, "%0D%0A") : "");
+
+function openShareUrl(e, type, eventUrl) {
+  e.stopPropagation();
+  let base;
+
+  switch (type) {
+    case 'facebook':
+      base = 'https://facebook.com/sharer/sharer.php?u=';
+      break;
+
+    case 'linkedin':
+      base = 'https://www.linkedin.com/sharing/share-offsite/?url=';
+      break;
+
+    case 'twitter':
+      base = 'http://twitter.com/share?url=';
+      break;
+
+    default:
+      console.error('undefined share url type');
+  }
+
+  window.open(base + eventUrl, "_blank");
+  return;
+}
+
+function generateEventUrl(event, encode, boomEventUrlBase, comp_id, instance) {
+  if (event.kind === 4) {
+    return event.eventPageUrl || '';
+  } else {
+    console.log(event.start);
+    return "".concat(boomEventUrlBase).concat(encodeShareUrl("".concat(event.id)), "?").concat(encode ? encodeURIComponent("comp_id=".concat(comp_id, "&instance=").concat(instance, "&startDate=").concat(event.repeat.type ? moment(event.start).format('YYYY-MM-DD') : "")) : "comp_id=".concat(comp_id, "&instance=").concat(instance), "&startDate=").concat(event.repeat.type ? moment(event.start).format('YYYY-MM-DD') : "");
+  }
+}
+
+function copyLink(e, event, setCopyTooltipText, copiedTooltipText, boomEventUrlBase, comp_id, instance) {
+  e.stopPropagation();
+  let a = document.createElement('textarea');
+  a.innerText = generateEventUrl(event, false, boomEventUrlBase, comp_id, instance);
+  document.body.appendChild(a);
+  a.setSelectionRange(0, 99999);
+  a.select();
+  document.execCommand('copy');
+  a.remove();
+  setCopyTooltipText(copiedTooltipText);
+}
--- src/components/AddToCalendarIcons/src/lib/main.js
+++ src/components/AddToCalendarIcons/src/lib/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './main.css';
 import {
    downloadSharer,
@@ -10,50 +10,68 @@ import {
 
 export default function AddShareIcons(props) {
 
-   if(!props.showAddToIcons && !props.showShareIcons) return null;
+   const [ copyTooltipText, setCopyTooltipText ] = useState(props.copyActionTooltipText);
+   
+   
+   if(!props.showAddToIcons && !props.showShareIcons) return null; 
 
-   const addToTypes = [ 'google', 'outlook', 'icloud', 'yahoo' ];
-   const shareTypes = [ 'facebook', 'linkedin', 'twitter', 'copylink' ];
-
+   const addToTypes = [ 'google', 'outlook', 'icalendar', 'yahoo' ];
+   const shareTypes = [ 'facebook', 'linkedin', 'twitter', 'copyLink' ];
       
    const addIcons = props.showAddToIcons && addToTypes.map(type => {
       let clickHandler;
-      if(type === 'google'  || type === 'yahoo') clickHandler = e => downloadSharer(e, type, props.event);
-      if(type === 'outlook' || type === 'icloud') clickHandler = e => openAddToUrl(type, props.event);
+      if(type === 'google'  || type === 'yahoo') clickHandler = e => openAddToUrl(e, type, props.event);
+      if(type === 'outlook' || type === 'icalendar') clickHandler = e => downloadSharer(e, type, props.event);
       return (
-         <span 
-            className={ `bmct-addShare-icons ${props.addToIconsCustomClassNames.join(' ')}` }
+         <span
+            key={type} 
+            className={ `bmct-add-share-icon bmct-${type} ${props.addToIconsCustomClassNames.join(' ')}` }
             onClick={ clickHandler }
          />
       )
    })
 
 
-   const eventUrl = generateEventUrl(props.event, true, props.boomEventUrlBase);
+   const eventUrl = generateEventUrl(props.event, true, props.boomEventUrlBase, props.comp_id, props.instance);
 
    const shareIcons = props.showShareIcons && shareTypes.map(type => {
-      const clickHandler = type === 'copylink' ? e => copyLink(props.event) : e => openShareUrl(e, type, eventUrl);
+      const clickHandler = type === 'copyLink' ? e => copyLink(e, props.event, setCopyTooltipText, props.copiedTooltipText, props.boomEventUrlBase, props.comp_id, props.instance) : e => openShareUrl(e, type, eventUrl);
       return (
-         <span 
-            className={ `bmct-addShare-icons ${props.shareIconsCustomClassNames.join(' ')}` }
+         <span
+            key={type} 
+            className={ `bmct-add-share-icon bmct-${type} ${props.shareIconsCustomClassNames.join(' ')}` }
             onClick={ clickHandler }
-         />
+            onMouseOut={ () => type === 'copyLink' && setCopyTooltipText(props.copyActionTooltipText) }
+         >
+            {
+               type === 'copyLink' &&
+               <span className='copy-tooltip'>{copyTooltipText}</span>
+            }
+         </span>
+
       )
    })
 
-
    return (
-      <>
-         <div className="bmct-icons-container">
-            <div>{ props.addToSectionTitle }</div>
-            <div>{ addIcons }</div>
-         </div>
-         <div className="bmct-icons-container">
-            <div>{ props.shareSectionTitle }</div>
-            <div>{ shareIcons }</div>
-         </div>
-      </>
-
+      <div className={`bmct-icons-${props.sequence}`}>
+         {
+            props.showAddToIcons ?
+            <div className={`bmct-icons-container ${props.iconsSectionCustomClassNames.join(' ')}`}>
+               <div>{ props.addToSectionTitle }</div>
+               <div>{ addIcons }</div>
+            </div> :
+            null
+         }
+         {
+            props.showShareIcons ?
+            <div className={`bmct-icons-container ${props.iconsSectionCustomClassNames.join(' ')}`}>
+               <div>{ props.shareSectionTitle }</div>
+               <div>{ shareIcons }</div>
+            </div> :
+            null          
+         }
+      </div>
    )
-
 }
+
+
